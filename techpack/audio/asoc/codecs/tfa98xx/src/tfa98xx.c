59cd22a38828062d666217d64c9e2edbb34faa1c
xNombre <kartapolska@gmail.com>
2021-09-24 22:49:49 +0200

    techpack: tfa98xx: Fix compilation when debugfs is disabled
    Signed-off-by: xNombre <kartapolska@gmail.com>
    Change-Id: Ie4d8df25e9502b58788d28f34bb6e9ac0c1f7dc6

--- techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
+++ techpack/audio/asoc/codecs/tfa98xx/src/tfa98xx.c
@@ -393,6 +393,16 @@ static void tfa98xx_inputdev_unregister(struct tfa98xx *tfa98xx)
 	__tfa98xx_inputdev_check_register(tfa98xx, true);
 }
 
+#ifdef TFA_NON_DSP_SOLUTION
+extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
+#else
+int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
+{
+	pr_info("this function is empty!!!\n");
+	return 0;
+}
+#endif
+
 #ifdef CONFIG_DEBUG_FS
 /* OTC reporting
  * Returns the MTP0 OTC bit value
@@ -744,16 +754,6 @@ static ssize_t tfa98xx_dbgfs_fw_state_get(struct file *file,
 	return simple_read_from_buffer(user_buf, count, ppos, str, strlen(str));
 }
 
-#ifdef TFA_NON_DSP_SOLUTION
-extern int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead);
-#else
-int send_tfa_cal_apr(void *buf, int cmd_size, bool bRead)
-{
-	pr_info("this function is empty!!!\n");
-	return 0;
-}
-#endif
-
 static ssize_t tfa98xx_dbgfs_rpc_read(struct file *file,
 				     char __user *user_buf, size_t count,
 				     loff_t *ppos)
@@ -986,7 +986,6 @@ static void tfa98xx_debug_remove(struct tfa98xx *tfa98xx)
 		debugfs_remove_recursive(tfa98xx->dbg_dir);
 }
 #endif
-
 
 /* copies the profile basename (i.e. part until .) into buf */
 static void get_profile_basename(char* buf, char* profile)
