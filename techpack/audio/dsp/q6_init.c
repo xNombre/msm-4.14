82db484ffd68fa9b8bfd2b502059eca9921dec5e
Andrzej Perczak <linux@andrzejperczak.com>
2021-11-15 23:10:53 +0100

    techpack: audio: Add Kconfig entry for elliptic driver
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: Ife38969db1ba53076fef8555794de800ed76be9c

--- techpack/audio/dsp/q6_init.c
+++ techpack/audio/dsp/q6_init.c
@@ -33,7 +33,9 @@ static int __init audio_q6_init(void)
 	avtimer_init();
 	msm_mdf_init();
 	voice_mhi_init();
+#ifdef CONFIG_ELLIPTIC
 	elliptic_driver_init();
+#endif
 #ifdef CONFIG_US_PROXIMITY
 	mius_driver_init();
 #endif
@@ -57,7 +59,9 @@ static void __exit audio_q6_exit(void)
 	audio_cal_exit();
 	adsp_err_exit();
 	voice_mhi_exit();
+#ifdef CONFIG_ELLIPTIC
 	elliptic_driver_exit();
+#endif
 #ifdef CONFIG_US_PROXIMITY
 	mius_driver_exit();
 #endif
