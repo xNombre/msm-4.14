c31a174dea6ecc154fd1399fb3850759d7296050
Andrzej Perczak <linux@andrzejperczak.com>
2021-11-18 16:25:35 +0100

    techpack: audio: Remove some frequent loggers
    Signed-off-by: Andrzej Perczak <linux@andrzejperczak.com>
    Change-Id: Iab584470d84d2ed445f0d89e5e6e72e34bd20bb8

--- techpack/audio/dsp/q6adm.c
+++ techpack/audio/dsp/q6adm.c
@@ -2851,7 +2851,7 @@ int adm_open(int port_id, int path, int rate, int channel_mode, int topology,
 	void *adm_params = NULL;
 	int param_size;
 
-	pr_info("%s:port %#x path:%d rate:%d mode:%d perf_mode:%d,topo_id 0x%x bit_width %d app_type %d acdb_id %d\n",
+	pr_debug("%s:port %#x path:%d rate:%d mode:%d perf_mode:%d,topo_id 0x%x bit_width %d app_type %d acdb_id %d\n",
 		__func__, port_id, path, rate, channel_mode, perf_mode,
 			 topology, bit_width, app_type, acdb_id);
 
